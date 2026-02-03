3c27987f71817b2884e42ff2577b92c9ea654e96
AndrewOgasMATC <ogasa@matc.edu>
2026-02-03 04:13:30 -0600

    Completed Activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -9,10 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog`,
+    description: ``,
+    author: `Andrew Ogas`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact: {
+      name: "Andrew Ogas",
+      company: "AODesignworks",
+      address: "123 N Fake St, Nowhere, KS",
+    },
   },
   plugins: [
     `gatsby-plugin-image`,
